08df20d7a5d35d285e08fa242e85de1c1870afe3
unknown <xiiguilhermeiix@gmail.com>
2025-06-08 16:50:01 -0300

    acoplando frontend e backend e implementando as funçoes

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
+const cors = require('cors')
 const app = express()
 const Redis = require('ioredis');
 
@@ -19,9 +20,11 @@ const redis = new Redis({
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors({origin: process.env.ALLOWED_ORIGIN, credentials: true,
-      allowedHeaders: ['Content-Type', 'Authorization']
-}))
+
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGIN,
+  // credentials: true
+}));
 
 app.get('/health', (req,res)=> {
     res.send('ok')
@@ -44,20 +47,30 @@ async function generateUniqueKey(url) {
 
   do {
     key = generateKey();
+    exists_on_my_links = (key == '0000' || key == '0001'  || key == '0002' || key == '0003' || key == '0004'
+|| key == '0005'
+    )
     exists = await redis.exists(key);
-  } while (exists);
+  } while (exists_on_my_links || exists);
 
-  await redis.set(key, url); 
-  return key;
+  const ok = await redis.set(key, url, 'EX', 60 * 60 * 24 * 7); 
+  if (ok) return key;
+  return ok;
 }
 
 
 
 const myUrls = [
-    { original: '', short: '' }
+    { original: process.env.GITHUB, short: '0000' },
+    { original: process.env.LINKEDIN, short: '0001' },
+    { original: process.env.PROJETO_FILMES, short: '0002' },
+    { original: process.env.PROJETO_AWS_IMAGE_PROCESSING, short: '0003' },
+    { original: process.env.PROJETO_AWS_LOAD_BALANCING, short: '0004' },
+    { original: process.env.PROJETO_REDUX, short: '0005' },
 ]
 
-app.post('/shorturl', (req,res)=> {
+
+app.post('/shorturl', async (req,res)=> {
     const { url } = req.body
     if (!url) {
         return res.status(400).json({ error: "URL não providenciada." });
@@ -65,19 +78,32 @@ app.post('/shorturl', (req,res)=> {
     if (typeof url !== 'string') {
         return res.status(400).json({ error: "URL deve ser uma string." });
     }
-    if (!url.startsWith('http://') || !url.startsWith('https://')) {
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
         return res.status(400).json({ error: "URL deve começar com http:// ou https://" });
+      }
+      if (url == 'http://' || url == 'https://') {
+      return res.status(400).json({ error: "URL incompleta." });
     }
 
-    generateUniqueKey(url).then(key => {
-        return res.status(200).json({link: `https://${process.env.DOMAIN}/${key}`})
-    });
+    const key = await generateUniqueKey(url)
+    if (key){
+      return res.status(200).json({link: `https://${process.env.DOMAIN}/${key}`})
+    } else {
+      return res.status(400).json({ error: "Erro inesperado. Tente novamente." });
+    }
+ 
 
-    return res.status(400).json({ error: "Erro inesperado. Tente novamente." });
 })
 
 app.get('/:short', async (req,res) => {
     const {short} = req.params;
+    if (!short) {
+      return res.status(400).json({ error: "URL Não encontrada" });
+    }
+    const foundLocally = myUrls.find(el => el.short == short);
+    if (foundLocally){
+        return res.redirect(302, foundLocally.original)
+    }
     const long_url = await redis.get(short)
     if (!long_url) {
         return res.status(400).json({ error: "URL Não encontrada" });
@@ -86,7 +112,7 @@ app.get('/:short', async (req,res) => {
     }
 })
 
-app.listen(80, () => {console.log('upon80')})
+app.listen(process.env.WEB_PORT, () => {console.log('upon80')})
 
 // https.createServer(options, app).listen(process.env.PORT, () => {
 //   console.log('UpOn443');
---
+++ frontend/src/App.jsx
@@ -0,0 +1,167 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment, useState } from 'react';
+
+
+function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState(
+    {error: 
+    {
+      status: false,
+      message: null
+    },
+     data: {
+      status: false,
+      message: null
+    }});
+      const [copied, setCopied] = useState(false);
+
+       const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(response.data.message);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 1500); // reset after 2 sec
+    } catch (err) {
+      console.error('Failed to copy!', err);
+    }
+  };
+
+  const handleKeyDown = async (e) => {
+    if (e.key === 'Enter') {
+      await handleRequest();
+    }
+  };
+
+  async function handleRequest(){
+    setLoading(true)
+    if (!(inputValue.trim())){
+
+      setLoading(false)
+       setInputValue('')
+        setResponse((prev) => {
+        return {...prev, error: {status: true, message: 'Campo de URL Vazio'}, data: {status: false, message: null }}
+      })
+    }
+    if (!inputValue.startsWith('http://') && !inputValue.startsWith('https://')){
+       setLoading(false)
+       setInputValue('')
+        setResponse((prev) => {
+        return {...prev, error: {status: true, message: 'URL Malformatada'}, data: {status: false, message: null }}
+      })
+    }
+    await fireRequest()
+    setLoading(false)
+            setInputValue('')
+  }
+
+  async function fireRequest(){
+    const res = await fetch('https://guilhermegavioli.xyz/shorturl', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({url: inputValue}),
+    })
+    console.log(res.status);
+    if (res.status == 200) {
+      const { link } = await res.json()
+      setResponse((prev) => {
+        return {...prev, data: {status: true, message: link}, error: {status: false, message: null }}
+      })
+    } else {
+         const { error } = await res.json()
+ 
+       setResponse((prev) => {
+        return {...prev, error: {status: true, message: error}, data: {status: false, message: null }}
+      })
+    }
+    
+  }
+
+
+  return (
+    <div style={{}}>
+
+<div style={{position: 'absolute', top: '10px', left: '10px', display: 'flex', alignItems: 'center', gap: '3px',
+  justifyContent: 'center'
+}}>
+
+      <p style={{userSelect: 'none',
+      fontSize: '1.2em',
+      color: 'rgb(225,225,225)'}}>
+
+          Guilherme Gavioli</p>
+           </div>
+
+      <p style={{position: 'absolute', bottom: '10px', right: '10px',
+      fontSize: '1.1em', userSelect: 'none', 
+         color: 'rgb(205,205,205)'}}>Demo App</p>
+
+      <div style={{position: 'absolute', inset: '0 0 120px 0', margin: 'auto', height: 'fit-content',
+        width: 'fit-content', display: 'flex', flexDirection: 'column',
+        alignItems: 'center', justifyContent: 'center'
+      }}>
+
+     {/* <h1 style={{fontSize: '2.4em'}}>ENCURTADOR DE URL</h1> */}
+     {/* <h2>Guilherme Gavioli</h2> */}
+     <div style={{height: '70px',width: '100%', backgorund: 'red', padding: '10px',
+      display: 'flex'
+     }}>
+     <input id="main-input"
+     style={{height: '50px', width: '500px', padding: '10px 15px', fontSize: '1.3em',
+      border: '1px solid rgb(207, 207, 207)'
+    }} placeholder='https://google.com/search?q=automata'
+    value={inputValue} 
+    onChange={(e) => setInputValue(e.target.value)}
+     onKeyDown={handleKeyDown}
+    />
+     <button onClick={handleRequest} style={{
+       background: loading ? '#1565c0' : '#1976d2' ,  border: 'none', padding: '10px 30px',
+        height: '50px', width: '160px',color: 'white',fontSize: '1.5em',
+       
+      }}>
+      {loading? 
+      <div className='my-spinner'></div>
+      :
+        <Fragment>
+        Encurtar
+        </Fragment>
+        }
+      </button>
+
+     </div>
+
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: '10px', alignItems: 'center', background: 'yellow',
+  
+         }}>
+          {
+            response.data.status && 
+            <>
+                <p style={{fontSize: '1.5em',  padding: '8px 15px'}}>{response.data.message}
+
+        </p>
+        {
+          copied ?
+          <p style={{marginRight: '15px'}}>copied!</p>
+          :
+          <img onClick={handleCopy} src="/copy-link.png" style={{width: '26px', height: '26px', marginRight: '15px'}} alt="" />
+        }
+            </>
+          }
+ 
+ {
+response.error.status && 
+  <p style={{userSelect: 'none', fontSize: '1.5em',  background: 'yellow', padding: '8px 15px'}}>{response.error.message}</p>
+
+}
+     </div>
+   
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
